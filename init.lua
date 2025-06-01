38ad3c50598160307989c0b4a2d2fc93bfe2ffe7
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-06-01 19:47:09 +0700

    docs: change init.lua tab appearance annotation

--- init.lua
+++ init.lua
@@ -9,9 +9,9 @@ vim.cmd.colorscheme("catppuccin")
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Set appearance of tabs to 4 spaces
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+-- Set appearance of tabs to 4 spaces only visually
+vim.opt.tabstop = 4		-- appearance of tab character
+vim.opt.shiftwidth = 4	-- number of spaces in auto indent new line
 
 -- Ruler lines
 vim.opt.colorcolumn = "81,101,121"
