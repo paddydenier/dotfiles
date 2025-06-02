42ef29c507cb10819975b2952f3c75121df0980e
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-06-02 20:28:43 +0700

    feat: add autopair rule to match $ in tex files

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,23 +1,24 @@
 return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = function()
-        local npairs = require("nvim-autopairs")
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = function()
+		local npairs = require("nvim-autopairs")
 
-        npairs.setup({})
+		npairs.setup({})
 
-        -- Add custom LaTeX pair for \[ \]
-        local Rule = require("nvim-autopairs.rule")
-        npairs.add_rules({
-            Rule("\\[", "\\]", "tex") -- apply only in LaTeX files
-                :with_pair(function(opts)
-                    -- Only add closing if not followed by ']'
-                    local next_char = opts.line:sub(opts.col + 1, opts.col + 1)
-                    return next_char ~= "]"
-                end)
-                :with_move(function(opts)
-                    return opts.prev_char:match(".%]") ~= nil
-                end)
-        })
-    end
+		-- Add custom LaTeX pair for \[ \]
+		local Rule = require("nvim-autopairs.rule")
+		npairs.add_rules({
+			Rule("\\[", "\\]", "tex") -- apply only in LaTeX files
+				:with_pair(function(opts)
+					-- Only add closing if not followed by ']'
+					local next_char = opts.line:sub(opts.col + 1, opts.col + 1)
+					return next_char ~= "]"
+				end)
+				:with_move(function(opts)
+					return opts.prev_char:match(".%]") ~= nil
+				end),
+			Rule("$", "$", "tex")
+		})
+	end,
 }
