1d7ac72307a19217f86edbbb75d253b4e1968e97
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-06-03 18:59:59 +0700

    feat: add keymap for telescope theme switching

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -17,6 +17,7 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find f
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.api.nvim_set_keymap("n", "<leader>ft", ":Telescope colorscheme<CR>", { noremap = true, silent = true })
 
 -- open code actions
 vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
