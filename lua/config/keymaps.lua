e8a4410bc82a07ac00821e14efdc99cc44c8af1b
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-06-11 13:28:22 +0700

    feat: improve pause workflow keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,6 +22,6 @@ vim.api.nvim_set_keymap("n", "<leader>ft", ":Telescope colorscheme<CR>", { norem
 -- open code actions
 vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
 
--- pause nvim workflow
-vim.keymap.set("n", "<leader>ww", ":w<CR><C-z>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>wq", ":wq<CR>", { noremap = true, silent = true })
+-- nvim pause workflow
+vim.keymap.set("n", "<leader>w", ":w<CR><C-z>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<leader>wq", ":wq<CR>", { noremap = true, silent = true })
