e2f02403edafe231d3fd60367e057af4f8226e70
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-06-11 19:03:21 +0700

    chore: set lualine to default

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,9 +3,9 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional for icons
 	config = function()
 		require("lualine").setup({
-			sections = {
-				lualine_a = { "lsp_status" },
-			},
+-- 			sections = {
+-- 				lualine_a = { "lsp_status" },
+-- 			},
 		})
 	end,
 }
