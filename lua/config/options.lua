373f0d5fa9b61360e08a48f3a69d68011b6dcbe4
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-06-11 13:12:49 +0700

    refactor: move init.lua vim options to separate config file

---
+++ lua/config/options.lua
@@ -0,0 +1,16 @@
+-- Relative and current line number
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Set appearance of tabs to 4 spaces only visually
+vim.opt.tabstop = 4    -- appearance of tab character
+vim.opt.shiftwidth = 4 -- number of spaces in auto indent new line
+
+-- Ruler lines
+vim.opt.colorcolumn = "81,101,121"
+
+-- Nowrap
+vim.opt.wrap = false
+
+-- disable startup message
+vim.opt.shortmess:append({ I = true })
