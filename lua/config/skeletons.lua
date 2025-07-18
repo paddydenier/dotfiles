77e3b7474eb38b64e021d0f9a6ec3b402df87b7e
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-07-18 21:27:53 +0700

    feat: add skeleton for tex-fmt.toml and renaming

--- lua/config/skeletons.lua
+++ lua/config/skeletons.lua
@@ -1,8 +1,6 @@
-vim.api.nvim_create_autocmd("BufNewFile", {
-  pattern = { "*.cpp", "*.cc" },
-  callback = function()
-    vim.cmd('0r ~/.config/nvim/skeletons/skeleton.cpp')
-  end,
-})
-
-
+-- vim.api.nvim_create_autocmd("BufNewFile", {
+--   pattern = { "*.cpp", "*.cc" },
+--   callback = function()
+--     vim.cmd('0r ~/.config/nvim/skeletons/skeleton.cpp')
+--   end,
+-- })
