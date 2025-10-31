d510d76c82d896a2aa2b6606ea5965cacec35257
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-10-31 19:56:48 +0700

    feat: add nightfox theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -4,7 +4,7 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 	},
-	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false,  priority = 1000 },
 	{
 		"navarasu/onedark.nvim",
 		priority = 1000, -- make sure to load this before all the other start plugins
@@ -16,5 +16,6 @@ return {
 			require("onedark").load()
 		end,
 	},
-	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+	{ "ellisonleao/gruvbox.nvim",  priority = 1000,  config = true, opts = ... },
+	{ "EdenEast/nightfox.nvim" }, -- lazy
 }
