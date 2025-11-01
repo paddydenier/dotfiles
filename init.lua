4c961fba2a29e3c13f7ad6987a044727a7154208
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-11-01 15:55:13 +0700

    feat: change default theme

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require("config.lazy")
 require("config.keymaps")
 require("config.options")
 require("config.skeletons")
-vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("carbonfox")
 
 -- diagnostics
 vim.opt.signcolumn = "yes"
