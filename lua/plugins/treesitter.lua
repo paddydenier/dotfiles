04af1f23f9e0c0f2b88fd79be4da29b50d6dd9c5
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-11-02 12:42:51 +0700

    chore: format treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,22 +2,22 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
-		require'nvim-treesitter.configs'.setup {
+		require("nvim-treesitter.configs").setup({
 			ensure_installed = { "c", "lua", "python", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 			sync_install = false,
 			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 			auto_install = false,
-  			highlight = {
-    			enable = true,
+			highlight = {
+				enable = true,
 				disable = function(lang, buf)
-        		local max_filesize = 100 * 1024 -- 100 KB
-        		local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        		if ok and stats and stats.size > max_filesize then
-             		return true
-        		end
-    		end,
-			additional_vim_regex_highlighting = false,
-  			},
-		}
-	end
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+				additional_vim_regex_highlighting = false,
+			},
+		})
+	end,
 }
