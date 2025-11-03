e77f04133e30f435bc499026dbb47bb35e15c4b3
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-11-04 00:10:47 +0700

    fix: use correct keymap call

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,7 +16,7 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find f
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-vim.api.nvim_set_keymap("n", "<leader>ft", ":Telescope colorscheme<CR>")
+vim.keymap.set("n", "<leader>ft", ":Telescope colorscheme<CR>")
 vim.keymap.set(
 	"n",
 	"<leader>fs",
