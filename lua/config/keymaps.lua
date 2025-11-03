863181cb298dd54871025fe5edb65a4420e1fafc
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-11-03 20:57:35 +0700

    chore: configure keymaps and update lazy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,7 +8,6 @@ vim.keymap.set("n", "<leader>la", "<cmd>Lazy update<CR>", { desc = "Update lazy 
 
 -- Oil keymaps (change to shorter keymaps)
 vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory in Oil" })
-vim.keymap.set("n", "<leader>o", "<cmd>Oil<CR>", { desc = "Open parent directory in Oil" })
 vim.keymap.set("n", "<leader>vim", "<cmd>Oil ~/.config/nvim<CR>", { desc = "Open neovim config directory in oil" })
 vim.keymap.set("n", "<leader>code", "<cmd>Oil ~/code<CR>", { desc = "Open code project directory in oil" })
 
@@ -19,18 +18,19 @@ vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live gr
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 vim.api.nvim_set_keymap("n", "<leader>ft", ":Telescope colorscheme<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>fs", "<cmd>NewFromTemplate<CR>", { desc = "Insert Skeleton Template", noremap = true, silent = true })
+vim.keymap.set(
+	"n",
+	"<leader>fs",
+	"<cmd>NewFromTemplate<CR>",
+	{ desc = "Insert Skeleton Template", noremap = true, silent = true }
+)
 
--- open code actions
-vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Show Line Diagnostics" })
-
--- nvim pause workflow
-vim.keymap.set("n", "<leader>s", ":w<CR><C-z>", { noremap = true, silent = true })
--- vim.keymap.set("n", "<leader>wq", ":wq<CR>", { noremap = true, silent = true })
+-- TODO: this is native now, read man page
+-- vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Show Line Diagnostics" })
 
 -- basic buffer switching
-vim.keymap.set("n", "<leader><tab>", "<C-^>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<leader><tab>", "<C-^>", { noremap = true, silent = true })
 
 -- quick and dirty print to pdf command keymap
 vim.keymap.set("n", "<leader>pp", function()
@@ -46,11 +46,9 @@ vim.keymap.set("n", "<leader>pp", function()
 	vim.cmd("silent !zathura " .. pdf_path .. " &") -- open PDF with Zathura
 end, { desc = "Export buffer to PDF and open in Zathura" })
 
-
-vim.keymap.set('n', '<leader>o', ':source<CR> :update<CR>')
-
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-
-vim.keymap.set('n', '<leader>q', ':q!<CR>')
-
-vim.keymap.set('n', '<leader>wq', ':wq<CR>')
+-- important keymaps
+vim.keymap.set("n", "<leader>o", ":source<CR> :update<CR>")
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>q", ":q!<CR>")
+vim.keymap.set("n", "<leader>wq", ":wq<CR>")
+vim.keymap.set("n", "<leader>s", ":w<CR><C-z>")
