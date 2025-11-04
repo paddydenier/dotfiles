9722ea6d76745ca3052a3375587e7a522a7ba961
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-11-04 18:47:04 +0700

    feat: add proper format support for none lsp tools

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -1,6 +1,16 @@
 return {
+	-- auto set up installed lsp servers from mason
 	"mason-org/mason-lspconfig.nvim",
-	opts = {},
+	opts = {
+		ensure_installed = {
+			"lua_ls",
+			"stylua",
+			"texlab",
+			"clangd",
+			"rust_analyzer",
+			"pyright",
+		},
+	},
 	dependencies = {
 		{ "mason-org/mason.nvim", opts = {} },
 		"neovim/nvim-lspconfig",
