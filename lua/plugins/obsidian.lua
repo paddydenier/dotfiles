b477fb976ab31843811c8713e0533d1f042307d4
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-11-05 19:03:25 +0700

    feat: add git-signs plugin and obsidian frontmatter

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -26,5 +26,26 @@ return {
 		},
 		notes_subdir = "notes",
 		new_notes_location = "notes_subdir",
+		notes = {
+			folder = "notes",
+			template = "notes_template.md",
+		},
+		templates = {
+			folder = "templates",
+			date_format = "%Y-%m-%d",
+		},
+		note_id_func = function(title)
+			-- If you provide a title, use it as the filename
+			return title and title:gsub("%s+", " ") or tostring(os.time())
+		end,
+		frontmatter = {
+			enabled = true,
+			func = function(note)
+				return {
+					title = note.title,
+					created = os.date("%Y-%m-%d"),
+				}
+			end,
+		},
 	},
 }
