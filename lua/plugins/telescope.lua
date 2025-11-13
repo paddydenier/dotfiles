57f7fcaa51fc510d584b8bbd61f72473a7ab9a44
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-11-14 00:54:32 +0700

    fix: call telescope setup once and change layout

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,31 +2,37 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-ui-select.nvim" },
 		config = function()
 			local telescope = require("telescope")
 			local actions = require("telescope.actions")
 			local action_state = require("telescope.actions.state")
+			local themes = require("telescope.themes")
 
+			-- Setup telescope once, including ui-select extension
 			telescope.setup({
-				defaults = {
+				defaults = themes.get_ivy({
+					previewer = true,
+					sorting_strategy = "ascending",
 					mappings = {
-						i = {
-							["<C-d>"] = actions.delete_buffer,
-						},
-						n = {
-							["<C-d>"] = actions.delete_buffer,
-						},
+						i = { ["<C-d>"] = actions.delete_buffer },
+						n = { ["<C-d>"] = actions.delete_buffer },
 					},
+				}),
+				extensions = {
+					["ui-select"] = themes.get_dropdown({}),
 				},
 			})
+
+			-- Load extensions
+			telescope.load_extension("ui-select")
 
 			-- Template picker function
 			local function pick_template_and_insert()
 				require("telescope.builtin").find_files({
 					prompt_title = "Select Template",
 					cwd = "~/.config/nvim/skeletons",
-					attach_mappings = function(prompt_bufnr, map)
+					attach_mappings = function(prompt_bufnr)
 						actions.select_default:replace(function()
 							local selection = action_state.get_selected_entry()
 							actions.close(prompt_bufnr)
@@ -39,19 +45,6 @@ return {
 
 			-- Create user command :NewFromTemplate
 			vim.api.nvim_create_user_command("NewFromTemplate", pick_template_and_insert, {})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
 		end,
 	},
 }
