0a64c618d42182393745afd1f398103ceb111055
Paddy Denier <202327146+paddydenier@users.noreply.github.com>
2025-12-11 18:29:41 +0700

    fix: convert open-github function to script

--- bash/.config/bash/scripts/open-github.sh
+++ bash/.config/bash/scripts/open-github.sh
@@ -1,22 +1,19 @@
 ##!/usr/bin/env bash
 
-function og() {
-	url=$(git remote get-url origin 2>/dev/null)
-	# Exit if git failed
-	if [[ $? -ne 0 ]]; then
-		echo "failed to get git remote"
-		exit 1
-	fi
+url=$(git remote get-url origin 2>/dev/null)
+# Exit if git failed
+if [[ $? -ne 0 ]]; then
+	echo "failed to get git remote"
+	exit 1
+fi
 
-	if [[ $url == *github.com* ]]; then
-		if [[ $url == git@* ]]; then
-			url="${url#git@}"
-			url="${url/:/\/}" 
-			url="https://$url"
-		fi
-		explorer.exe "$url"
-	else
-		echo "This repository is not hosted on GitHub"
+if [[ $url == *github.com* ]]; then
+	if [[ $url == git@* ]]; then
+		url="${url#git@}"
+		url="${url/:/\/}" 
+		url="https://$url"
 	fi
-}
-
+	explorer.exe "$url"
+else
+	echo "This repository is not hosted on GitHub"
+fi
